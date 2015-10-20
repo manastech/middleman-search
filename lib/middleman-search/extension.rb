c9e4ee874fc7687f97210b54854cd6b79cf6c342
Matias Garcia Isaia <mgarcia@manas.com.ar>
2015-10-20 19:52:49 -0300

    Customisable index file path

--- lib/middleman-search/extension.rb
+++ lib/middleman-search/extension.rb
@@ -5,10 +5,10 @@ module Middleman
   class SearchExtension < Middleman::Extension
     option :resources, [], 'Paths of resources to index'
     option :fields, {}, 'Fields to index, with their options'
+    option :index_path, 'search.json', 'Index file path'
 
     def manipulate_resource_list(resources)
-      index_file_path = 'index.json'
-      resources.push Middleman::Sitemap::SearchIndexResource.new(@app.sitemap, index_file_path, @options)
+      resources.push Middleman::Sitemap::SearchIndexResource.new(@app.sitemap, @options[:index_path], @options)
       resources
     end
   end

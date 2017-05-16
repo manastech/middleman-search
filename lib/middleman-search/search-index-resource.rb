6077d7b76d010ade046a32711536cab3601e4910
Matthijs Kuiper <info@matthijskuiper.nl>
2017-05-16 10:33:07 +0200

    Update to Lunr 2.0.3

--- lib/middleman-search/search-index-resource.rb
+++ lib/middleman-search/search-index-resource.rb
@@ -45,10 +45,14 @@ module Middleman
 
         # Register pipeline functions
         pipeline = context.eval('lunr.Pipeline')
+
         @pipeline.each do |name, function|
           context[name] = context.eval("(#{function})")
           pipeline.registerFunction(context[name], name)
         end
+
+        # Reference to resource map
+        store = Hash.new
 
         # Build lunr based on config
         lunr = context.eval('lunr')
@@ -68,40 +72,37 @@ module Middleman
             next if opts[:index] == false
             this.field(field, {:boost => opts[:boost]})
           end
+
+          # Iterate over all resources and build index
+          @app.sitemap.resources.each_with_index do |resource, id|
+            begin
+              catch(:skip) do
+                next if resource.data['index'] == false
+                next unless @resources_to_index.any? {|whitelisted| resource.path.start_with? whitelisted }
+
+                to_index = Hash.new
+                to_store = Hash.new
+
+                @fields.each do |field, opts|
+                  value = value_for(resource, field, opts)
+                  throw(:skip) if value.blank? && opts[:required]
+                  to_index[field] = value unless opts[:index] == false
+                  to_store[field] = value if opts[:store]
+                end
+
+                @callback.call(to_index, to_store, resource) if @callback
+
+                this.add(to_index.merge(id: id))
+                store[id] = to_store
+              end
+            rescue => ex
+              @app.logger.warn "Error processing resource for index: #{resource.path}\n#{ex}\n #{ex.backtrace.join("\n ")}"
+            end
+          end
         end
 
         # Get lunr index
         index = lunr.call(lunr_conf)
-
-        # Ref to resource map
-        store = Hash.new
-
-        # Iterate over all resources and build index
-        @app.sitemap.resources.each_with_index do |resource, id|
-          begin
-            catch(:skip) do
-              next if resource.data['index'] == false
-              next unless @resources_to_index.any? {|whitelisted| resource.path.start_with? whitelisted }
-
-              to_index = Hash.new
-              to_store = Hash.new
-
-              @fields.each do |field, opts|
-                value = value_for(resource, field, opts)
-                throw(:skip) if value.blank? && opts[:required]
-                to_index[field] = value unless opts[:index] == false
-                to_store[field] = value if opts[:store]
-              end
-
-              @callback.call(to_index, to_store, resource) if @callback
-
-              index.add(to_index.merge(id: id))
-              store[id] = to_store
-            end
-          rescue => ex
-            @app.logger.warn "Error processing resource for index: #{resource.path}\n#{ex}\n #{ex.backtrace.join("\n ")}"
-          end
-        end
 
         # Generate JSON output
         "{\"index\": #{index.indexJson()}, \"docs\": #{store.to_json}}"
@@ -118,7 +119,6 @@ module Middleman
       def value_for(resource, field, opts={})
         case field.to_s
         when 'content'
-
           html = resource.render( { :layout => false }, { :current_path => resource.path } )
           Nokogiri::HTML(html).xpath("//text()").text
         when 'url'
